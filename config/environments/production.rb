1a38f3761fed144eb390b7e672b77a757dead6c4
uniclown <clown139880@uniclowntheworld.c.clown139880.internal>
2017-11-09 08:18:02 +0000

    [uniclown]deploy on UCTWGCP

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,20 +26,6 @@ Rails.application.configure do
   config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-  host = 'uniclown-akagi-project.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
-  }
-
 
 
 
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,41 +1,63 @@
 module Rails
   class <<self
     def root
-      File.expand_path("../..", __FILE__)
+      File.expand_path(__FILE__).split('/')[0..-3].join('/')
     end
   end
 end
-puts Rails.root
-rails_env = ENV["RAILS_ENV"] || "development"
 
-preload_app true
+worker_processes 4
+
 working_directory Rails.root
+
+listen "#{Rails.root}/tmp/sockets/socket", :backlog => 64
+
+timeout 30
+
 pid "#{Rails.root}/tmp/pids/unicorn.pid"
+
 stderr_path "#{Rails.root}/log/unicorn.log"
 stdout_path "#{Rails.root}/log/unicorn.log"
 
-listen 4000, :tcp_nopush => false
-
-listen "#{Rails.root}/unicorn.sock"
-
-worker_processes 1
-timeout 60
-
-if GC.respond_to?(:copy_on_write_friendly=)
+# combine Ruby 2.0.0dev or REE with "preload_app true" for memory savings
+# http://rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
+preload_app true
+GC.respond_to?(:copy_on_write_friendly=) and
   GC.copy_on_write_friendly = true
-end
 
-before_exec do |server|
-  ENV["BUNDLE_GEMFILE"] = "#{Rails.root}/Gemfile"
-end
+check_client_connection false
 
 before_fork do |server, worker|
-  old_pid = "#{Rails.root}/tmp/pids/unicorn.pid.oldbin"
-  if File.exists?(old_pid) && server.pid != old_pid
+  # the following is highly recomended for Rails + "preload_app true"
+  # as there's no need for the master process to hold a connection
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exists?(old_pid) && old_pid != server.pid
     begin
-      Process.kill("QUIT", File.read(old_pid).to_i)
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
     rescue Errno::ENOENT, Errno::ESRCH
-          puts "Send 'QUIT' signal to unicorn error!"
     end
   end
+
+  # Throttle the master from forking too quickly by sleeping.  Due
+  # to the implementation of standard Unix signal handlers, this
+  # helps (but does not completely) prevent identical, repeated signals
+  # from being lost when the receiving process is busy.
+  sleep 1
+end
+
+after_fork do |server, worker|
+
+  # the following is *required* for Rails + "preload_app true",
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+
+  # if preload_app is true, then you may also want to check and
+  # restart any other shared sockets/descriptors such as Memcached,
+  # and Redis.  TokyoCabinet file handles are safe to reuse
+  # between any number of forked children (assuming your kernel
+  # correctly implements pread()/pwrite() system calls)
 end
